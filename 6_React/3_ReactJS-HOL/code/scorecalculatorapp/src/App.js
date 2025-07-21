b4618a985a48377458abc01f415b3ebce49fa253
Jashwanth <jashlinked@gmail.com>
2025-07-21 19:02:34 +0530

    week 6

---
+++ 6_React/3_ReactJS-HOL/code/scorecalculatorapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CalculateScore } from './Components/CalculateScore'
+
+const App = () => {
+  return (
+    <div>
+      <CalculateScore Name="Jash" School=
+        "AFD  School" total={122} goal={2}></CalculateScore>
+    </div>
+  )
+}
+
+export default App
